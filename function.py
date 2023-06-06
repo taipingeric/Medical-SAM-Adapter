a79889d6d80714fed60d733198f1ce75d7f6522a
CYLi <taipingeric@gmail.com>
2023-06-06 11:04:15 +0800

    fix

--- function.py
+++ function.py
@@ -16,7 +16,7 @@ from torch.utils.data import DataLoader
 #from dataset import *
 from torch.autograd import Variable
 from PIL import Image
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
 #from models.discriminatorlayer import discriminator
 from conf import settings
 import time
